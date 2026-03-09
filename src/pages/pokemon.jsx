8a663e46f3d31e165035c0b02c2a0a3c8bb17409
bryanhuang <bryancanmusic@gmail.com>
2026-03-09 00:02:18 -0700

    pokemon working

--- src/pages/pokemon.jsx
+++ src/pages/pokemon.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react'
+import { PokemonCard } from '../components/PokemonCard'
 
 export function Pokemon() {
+    const [pokemon, setPokemon] = useState([])
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+        .then(response => response.json())
+        .then(data => setPokemon(data.results))
+        .catch(error => console.error('Error fetching pokemon:', error))
+    }, [])
+
+    console.log(pokemon)
+
     return (
         <>
         <h1>POKEMON!!!!</h1>
-        <p>POKEMON!!!!</p>
+        {pokemon.map(pokemon => <PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url}/>)}
         </>
     )
 }
